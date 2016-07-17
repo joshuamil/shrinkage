bf1d054d7aa114cab3c1e1f240cc1487faf76a9f
Joshua Miller <joshuamil@gmail.com>
2016-07-16 23:32:40 -0400

    Pushing up some changes that allow for greater control over image manipulation

--- bin/methods.js
+++ bin/methods.js
@@ -1,5 +1,6 @@
 var sharp = require('sharp'),
     path = require('path'),
+    lodash = require('lodash'),
     actions = require(__dirname + '/../conf/actions.json').actions,
     config = require(__dirname + '/../conf/conf.json').config;
 
@@ -8,64 +9,90 @@ var self = module.exports = {
   processRequest: function(req, res){
 
     var options = {};
-    var cfg = {};
 
+    // Set the action for this request
     options.act = req.body.action || "default";
 
     // Get configuration using the specified action
-    for(var key in actions){
-      if(actions.hasOwnProperty(key)){
-        if(actions[key].hasOwnProperty("name") && actions[key]["name"] === options.act){
-          cfg = actions[key];
-          break;
-        }
-      }
+    var cfg = lodash.filter(actions, { 'name': '' + options.act })[0];
+
+    if(!cfg){
+      var cfg = lodash.filter(actions, { 'name': 'default' })[0];
     }
 
+    // Set options for image manipulation
     options.fmt = cfg.format || "png";
     options.name = req.body.filename || "output." + options.fmt;
     options.level = cfg.level || 7;
     options.size = cfg.size || "";
+    options.crop = "sharp.gravity." + cfg.crop || false;
+    options.extract = cfg.extract || false;
+    options.rotate = cfg.rotate || false;
+    options.invert = cfg.invert || false;
+    options.flip = cfg.flip || false;
+    options.flop = cfg.flop || false;
+    options.quality = cfg.quality || false;
 
+    // Set the image object
     var image = sharp(req.files.image.file);
 
+    // Process the image
     image
       .metadata()
       .then(
         function(metadata) {
 
-          if(options.size === ""){
-            size.w = metadata.wdith;
-            size.h = metadata.height;
+          // Resize the image
+          if(options.size !== "" && !options.extract){
+            image = image.resize(options.size.w,options.size.h)
           }
 
-          switch(options.act){
-
-            case "thumbnail":
-              self.processThumbnail(image, options);
-              break;
-
-            case "icon":
-              self.processIcon(image, options);
-              break;
-
-            case "avatar":
-              self.processAvatar(image, options);
-              break;
-
-            case "hero":
-              self.processHero(image, options);
-              break;
-
-            case "default":
-              self.processDefault(image, options);
-              break;
-
-            default:
-              self.processDefault(image, options);
-
+          // Crop the image
+          if(options.crop && options.crop !== false){
+            image = image.crop(sharp.strategy.entropy);
           }
 
+          // Rotate the image
+          if(options.rotate && options.rotate !== false){
+            image = image.rotate(options.rotate);
+          }
+
+          // Invert the image
+          if(options.invert){
+            image = image.negate();
+          }
+
+          // Mirror the image vertically
+          if(options.flip){
+            image = image.flip();
+          }
+
+          // Mirror the image horizontally
+          if(options.flop){
+            image = image.flop();
+          }
+
+          // Mirror the image horizontally
+          if(options.quality){
+            image = image.quality(options.quality);
+          }
+
+          // Extract a portion of the image
+          if(options.extract){
+            image = image
+              .extract({
+                left: options.extract.left,
+                top: options.extract.top,
+                width: options.size.w,
+                height: options.size.h
+              });
+          }
+
+          // Apply final changes to the image object
+          return image
+            .toFormat(options.fmt)
+            .compressionLevel(options.level)
+            .toFile('./tmp/' + options.name);
 
         },
 
@@ -77,52 +104,6 @@ var self = module.exports = {
         res.sendFile(path.join(__dirname, '../tmp', options.name));
       });
 
-  },
-
-
-  /* Process thumbnail images */
-  processThumbnail: function(image, options){
-    return image
-      .resize(options.size.w,options.size.h)
-      .toFormat(options.fmt)
-      .compressionLevel(options.level)
-      .toFile('./tmp/' + options.name);
-  },
-
-  /* Process icon images */
-  processIcon: function(image, options){
-    return image
-      .resize(options.size.w,options.size.h)
-      .toFormat(options.fmt)
-      .compressionLevel(options.level)
-      .toFile('./tmp/' + options.name);
-  },
-
-  /* Process avatar images */
-  processAvatar: function(image, options){
-    return image
-      .resize(options.size.w,options.size.h)
-      .toFormat(options.fmt)
-      .compressionLevel(options.level)
-      .toFile('./tmp/' + options.name);
-  },
-
-  /* Process hero images */
-  processHero: function(image, options){
-    return image
-      .resize(options.size.w,options.size.h)
-      .toFormat(options.fmt)
-      .compressionLevel(options.level)
-      .toFile('./tmp/' + options.name);
-  },
-
-  /* Process image */
-  processDefault: function(image, options){
-    return image
-      .resize(options.size.w,options.size.h)
-      .toFormat(options.fmt)
-      .compressionLevel(options.level)
-      .toFile('./tmp/' + options.name);
   }
 
 };
